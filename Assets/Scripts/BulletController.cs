9c4a94c4979cb6b370839e0908f1b20a257a907e
Kathryn <kathryn.alcalde@gmail.com>
2021-02-27 09:45:19 -0700

    Added Player Controls and Shooting

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    public float playerBulletSpeed = 5f;
+
+    
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * playerBulletSpeed * Time.deltaTime);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(this);
+    }
+}
