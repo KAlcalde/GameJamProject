0b7469e171b6e9fe06a972571c7af155400c319c
Kathryn <kathryn.alcalde@gmail.com>
2021-03-08 10:33:51 -0700

    player asset working, and camera movement added
    I believe I fixed the player so that the asset game object is not working and the place holder is gone. Also, I added the camera movement to follow the player to allow for exploration

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform cameraLookAt;
+
+    public float boundX = 2.0f;
+    public float boundZ = 1.5f;
+
+    public float lerpSpeed = 0.15f;
+
+    private Vector3 desiredPosition;
+
+    private void LateUpdate()
+    {
+        Vector3 delta = Vector3.zero;
+
+        float dx = cameraLookAt.position.x - transform.position.x;
+        if(dx > boundX || dx < -boundX)
+        {
+            if (transform.position.x < cameraLookAt.position.x)
+            {
+                delta.x = dx - boundX;
+            }
+            else
+            {
+                delta.x = dx + boundX;
+            }
+        }
+
+        float dz = cameraLookAt.position.z - transform.position.z;
+        if (dz > boundZ || dz < -boundZ)
+        {
+            if (transform.position.z < cameraLookAt.position.z)
+            {
+                delta.z = dz - boundZ;
+            }
+            else
+            {
+                delta.z = dz + boundZ;
+            }
+        }
+
+        //Move the camera
+        desiredPosition= transform.position + delta;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, lerpSpeed);
+    }
+}
